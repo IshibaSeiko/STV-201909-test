353d7f3fc85ce65964d67ec4b48cb68f6ec8e1a8
IshibaSeiko <seiko.ishiba@st-ventures.jp>
2019-09-21 09:55:49 +0900

    initial commit

---
+++ STV-201909-test/STV-201909-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STV-201909-test
+//
+//  Created by STV-M025 on 2019/09/21.
+//  Copyright © 2019 STV-M025. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
